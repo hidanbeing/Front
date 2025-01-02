d5c116c012b969ffab4665a031538e295fa7dce7
hidanbeing <nd6162bv@gmail.com>
2025-01-02 16:09:08 +0900

    chore : 라이브러리 설정 npm install react-router-dom npm install --save-dev @types/react-router-dom
    npm install sass

---
+++ src/containers/Home/Homs.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+      Home
+      </header>
+    </div>
+  );
+}
+
+export default Home;
